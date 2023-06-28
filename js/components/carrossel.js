97e220a7a7a5ad64376c960eaae8f4ba9fbdbff6
Bruno Dyego Silveira da Silva <bruno.ufra@yahoo.com.br>
2023-06-27 23:15:36 -0300

    Update carrossel

---
+++ js/components/carrossel.js
@@ -0,0 +1,14 @@
+const imgS = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel() {
+    idx++;
+    if(idx > img.length-1) {
+        idx=0;
+    }
+    imgS.style.transform='translateX(${-idx*200}px)';
+}
+
+setInterval(carrossel, 1800);
